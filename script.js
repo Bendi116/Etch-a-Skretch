d9a3caed43ffdf6efd9b9889f3742072b2b2a96f
Bendi116 <142507571+Bendi116@users.noreply.github.com>
2024-02-09 18:50:09 +0100

    Add new functions, that convert rgb to hsl and increase the lightness of hsl

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const container = document.querySelector("#container")
 const height = parseInt(window.getComputedStyle(container).height)
 const btn = document.querySelector(".btn")
 const randBtn = document.querySelector(".random-btn")
-
+const lightBtn = document.querySelector(".l-btn")
 
 
 
 //event listener
 btn.addEventListener("click",showPromptMenu)
 randBtn.addEventListener("click",toogleRandomized)
-
+lightBtn.addEventListener("click",lightIncreaseMode)
 
 //functions
 function createGrid(square){
@@ -43,7 +43,7 @@ function createGrid(square){
             col_div.style.height = convertToPX(col_height)
             col_div.style.width = convertToPX(col_height)
             col_div.style.border = "1px solid #999999"
-            col_div.style.backgroundColor = "black"
+            col_div.style.backgroundColor = "hsl(0,0%,0%)"
 
             //add event listener
             col_div.addEventListener("mouseenter",createHoverEffect)
@@ -69,23 +69,31 @@ function convertToPX(num){
 
 function createHoverEffect(e){
 
-    if(randBtn.classList.contains("active")){
+    if(randBtn.classList.contains("active") && !lightBtn.classList.contains("increase")){
         e.target.style.backgroundColor = randomizeColor()
+    }else if(!randBtn.classList.contains("active") && lightBtn.classList.contains("increase")){
+        e.target.style.backgroundColor = increaseLightness(e.target.style.backgroundColor)
     }else{
-        e.target.style.backgroundColor = "white"
+        e.target.style.backgroundColor = "hsl(0,0%,100%)"
     }
    
     
 }
 
 function removeHoverEffect(e){
+    
+    if(!lightBtn.classList.contains("increase") || (randBtn.classList.contains("active") && lightBtn.classList.contains("increase")))
+    {
         setTimeout(()=>{
-            e.target.style.backgroundColor = "black"
+            e.target.style.backgroundColor = "hsl(0,0%,0%)"
         },700)
+    }
+        
     
    
     
 }
+
 function showPromptMenu(){
     let input;
     do{
@@ -112,6 +120,85 @@ function toogleRandomized(e){
 
 }
 
-createGrid(50)
-randomizeColor()
+function lightIncreaseMode(){
+    lightBtn.classList.toggle("increase")
+    
+}
 
+function increaseLightness(color){
+    let rgb = getRGB(color)
+    hsl = RGBtoHSL(...rgb)
+    return changaLightness(10,hsl)
+
+}
+
+//from others
+function getRGB(rgb){
+    let rgb_array = rgb.split(",")
+    //r
+    let r = rgb_array[0].slice(rgb_array[0].indexOf("(")+1)
+
+    //g
+    let g = rgb_array[1].trim()
+    
+    //b
+    let b = rgb_array[2].slice(0,rgb_array[2].indexOf(")"))
+    return [r,g,b]
+}
+function RGBtoHSL(r,g,b){
+    //make r, g and b fractions of 1
+    r /= 255;
+    g /= 255;
+    b /= 255;
+
+    //find greatest and smallest channel values
+    let cmin = Math.min(r,g,b),
+        cmax = Math.max(r,g,b),
+        delta = cmax-cmin,
+        h = 0,
+        s = 0,
+        l = 0
+    
+    //calculate hue
+    //no difference
+    if (delta == 0) h = 0;
+    else if( cmax == r) h = ((g - b) / delta)%6;
+    else if(cmax == g)h = (b-r)/delta +2;
+    else h = (r - g)/delta + 4;
+
+    h = Math.round(h*60);
+
+    //make negatives hues positive behind 360°
+    if(h<0) h += 360;
+
+    //calculate ligtness
+    l = (cmax + cmin) / 2;
+
+    //calculate saturation
+    s = delta == 0 ?  0 : delta/(1- Math.abs(2 * l -1));
+
+    // Multiply l and s by 100
+    s = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+
+    return "hsl("+h+","+s+"%,"+l+"%)"
+
+
+    
+}
+function changaLightness(delta, hslStr){
+    const [hue, saturation, lightness] = hslStr.match(/\d+/g).map(Number);
+    
+    
+    const newLightness = Math.max(
+        0,
+        Math.min(100, lightness + Number.parseFloat(delta))
+    );
+
+    return `hsl(${hue},${saturation}%,${newLightness}%)`
+}
+//main
+
+
+createGrid(50)
+getRGB("rgb(100,100,100)")
